79f545ccf1dccfcfb08ba3fe84e33c47f85ce9a2
Chuck <chuck@Chucks-MacBook-Pro.local>
2026-06-02 23:05:47 -0400

    Add BASE_PATH global variable for github pages deployment paths

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// src/config.js
+// Changez cette valeur à '' (chaîne vide) lorsque le site sera sur votre propre nom de domaine.
+// Pour GitHub Pages, laissez '/tomy_lef'.
+export const BASE_PATH = '/tomy_lef';
