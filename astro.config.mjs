867c2b9c84b5bf7d5f1d14b25e24e7b88dc02bde
Chuck <chuck@Chucks-MacBook-Pro.local>
2026-06-02 23:00:16 -0400

    Remove base config to fix local dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://viesigners.github.io',
-  base: '/tomy_lef',
   srcDir: './src',
   publicDir: './public',
   build: {
